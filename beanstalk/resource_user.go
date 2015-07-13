988795959e7dcf9f5e90e2ff5341c8baf6839613
Martin Atkins <matkins@saymedia.com>
2015-07-13 15:20:50 -0700

    CRUD for resource_user.

--- beanstalk/resource_user.go
+++ beanstalk/resource_user.go
@@ -18,7 +18,6 @@ func resourceUser() *schema.Resource {
 			"username": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
-				ForceNew: true,
 			},
 
 			"name": &schema.Schema{
@@ -44,7 +43,7 @@ func resourceUser() *schema.Resource {
 			},
 
 			"id": &schema.Schema{
-				Type:     schema.TypeString,
+				Type:     schema.TypeInt,
 				Computed: true,
 			},
 
@@ -57,11 +56,6 @@ func resourceUser() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
-
-			"account_owner": &schema.Schema{
-				Type:     schema.TypeBool,
-				Computed: true,
-			},
 		},
 	}
 }
@@ -69,11 +63,13 @@ func resourceUser() *schema.Resource {
 func CreateUser(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*Client)
 
+	email := d.Get("email").(string)
+
 	req := &InvitationCreateRequestWrap{
 		InvitationCreateRequest{
 			User{
 				Name:  d.Get("name").(string),
-				Email: d.Get("email").(string),
+				Email: email,
 			},
 		},
 	}
@@ -84,29 +80,97 @@ func CreateUser(d *schema.ResourceData, meta interface{}) error {
 		return err
 	}
 
-	id := strconv.Itoa(res.Invitation.ID)
-	d.SetId(id)
+	// By creating an invitation we also created a user, but the
+	// Beanstalk API doesn't give us the id of the user in the
+	// response so we have to go hunt for it in the user list,
+	// using the email address (which is guaranteed unique).
+
+	id := 0
+	var ures []UserWrap
+	pathParts := []string{"users"}
+	queryArgs := map[string]string{
+		"per_page": "50",
+		"page":     "0",
+	}
+	pageIdx := 1
+Pages:
+	for id == 0 {
+		queryArgs["page"] = strconv.Itoa(pageIdx)
+		err := client.Get(pathParts, queryArgs, &ures)
+		if err != nil {
+			return err
+		}
+		if len(ures) == 0 {
+			return fmt.Errorf("invited user %v is not in user list", email)
+		}
+		for _, userWrap := range ures {
+			if userWrap.User.Email == email {
+				id = userWrap.User.ID
+				break Pages
+			}
+		}
+
+		pageIdx++
+	}
+
+	d.SetId(strconv.Itoa(id))
 	d.Set("id", id)
+
+	return UpdateUser(d, meta)
+}
+
+func ReadUser(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*Client)
+
+	var res UserWrap
+	err := client.Get([]string{"users", d.Id()}, nil, &res)
+	if err != nil {
+		if _, ok := err.(*NotFoundError); ok {
+			d.SetId("")
+			return nil
+		}
+		return err
+	}
+
+	d.Set("username", res.User.Username)
+	d.Set("email", res.User.Email)
+	d.Set("name", res.User.Name)
+	d.Set("account_admin", res.User.IsAccountAdmin)
+	d.Set("timezone", res.User.Timezone)
+	d.Set("first_name", res.User.FirstName)
+	d.Set("last_name", res.User.LastName)
 
 	return nil
 }
 
-func ReadUser(d *schema.ResourceData, meta interface{}) error {
-	//client := meta.(*Client)
-
-	return fmt.Errorf("ReadUser not yet implemented")
-}
-
 func UpdateUser(d *schema.ResourceData, meta interface{}) error {
-	//client := meta.(*Client)
+	client := meta.(*Client)
 
-	return fmt.Errorf("UpdateUser not yet implemented")
+	userWrap := &UserWrap{
+		User: User{
+			Username: d.Get("username").(string),
+			Email: d.Get("email").(string),
+			Name: d.Get("name").(string),
+			IsAccountAdmin: d.Get("account_admin").(bool),
+			Timezone: d.Get("timezone").(string),
+		},
+	}
+
+	err := client.Put([]string{"users", d.Id()}, userWrap, nil)
+	if err != nil {
+		return err
+	}
+
+	return ReadUser(d, meta)
 }
 
 func DeleteUser(d *schema.ResourceData, meta interface{}) error {
-	//client := meta.(*Client)
-
-	return fmt.Errorf("DeleteUser not yet implemented")
+	client := meta.(*Client)
+	err := client.Delete([]string{"users", d.Id()})
+	if err == nil {
+		d.SetId("")
+	}
+	return err
 }
 
 type Invitation struct {
@@ -129,7 +193,7 @@ type InvitationCreateRequestWrap struct {
 
 type User struct {
 	ID             int    `json:"id,omitempty"`
-	Username       string `json:"username"`
+	Username       string `json:"login"`
 	Email          string `json:"email"`
 	Name           string `json:"name"`
 	Timezone       string `json:"timezone"`
