727bda3e3654ca4f153ce47e0274da31c0005c31
Martin Atkins <matkins@saymedia.com>
2016-03-01 09:57:38 -0800

    Resource structure for modular webhook integrations

---
+++ beanstalk/resource_modular_webhook_integration.go
@@ -0,0 +1,64 @@
+package beanstalk
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceModularWebhookIntegration() *schema.Resource {
+	integrationType := &integrationType{
+		Name: "ModularWebhooksIntegration",
+		Attributes: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"service_url": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"triggers": &schema.Schema{
+				Type:     schema.TypeList,
+				MaxItems: 1,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"commit": &schema.Schema{
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"push": &schema.Schema{
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"deploy": &schema.Schema{
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"comment": &schema.Schema{
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"create_branch": &schema.Schema{
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"delete_branch": &schema.Schema{
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"create_tag": &schema.Schema{
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"delete_tag": &schema.Schema{
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return integrationType.resource()
+}
